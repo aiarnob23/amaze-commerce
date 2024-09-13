4c1fd7e17f429ddb0c72eec4d651d8ff268cf5d8
aiarnob23 <aiarnob23@gmail.com>
2024-09-13 15:54:42 +0600

    deploying issue again

--- amaze-commerce-frontend/lib/config.ts
+++ amaze-commerce-frontend/lib/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_BASE_URL ="https://amaze-commerce-backend-nk8ehvuqu-aminul-islam-arnobs-projects.vercel.app/api";
+export const SERVER_BASE_URL ="https://amaze-commerce-backend-aminul-islam-arnobs-projects.vercel.app/api";
 // export const SERVER_BASE_URL = "http://localhost:4000/api";
