7add6d066f0309f293e211db41d05edd1a2e4a05
aiarnob23 <aiarnob23@gmail.com>
2025-06-20 21:36:28 +0600

    updated urls

--- amaze-commerce-backend/src/app.ts
+++ amaze-commerce-backend/src/app.ts
@@ -16,16 +16,9 @@ const app: Application = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"https://amaze-commerce-front.vercel.app/",
   credentials:true,
 }));
-// app.use(
-//   cors({
-//     // origin: ["https://amaze-commerce-front.vercel.app","http://localhost:3000"],
-//     origin: "*",
-//     credentials: true,
-//   })
-// );
 
 //routes 
 app.use("/api/products", ProductsRoutes);
